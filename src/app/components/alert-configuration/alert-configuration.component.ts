9a7b36491685254cab3207328b9d2368ee0997cb
mwisnie <>
2018-09-08 21:34:21 +0200

    alert configuration updates - station map

--- src/app/components/alert-configuration/alert-configuration.component.ts
+++ src/app/components/alert-configuration/alert-configuration.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { User } from '../../model/User';
 import { Subscription } from 'rxjs';
+import { Station } from '../../model/Station';
+import { ExternalApiService } from '../../services/external-api.service';
+import { MeasurementPoint } from '../../model/MeasurementPoint';
 
 @Component({
   selector: 'app-alert-configuration',
@@ -11,12 +14,22 @@ import { Subscription } from 'rxjs';
 export class AlertConfigurationComponent implements OnInit, OnDestroy {
 
   user: User = null;
-  userSubscr: Subscription = null;
+  stations: Array<Station> = [];
+  userStationList: Array<Station> = [];
+  stationDetail: Array<MeasurementPoint> = [];
 
-  constructor(private authService: AuthenticationService) { }
+  isListFullAlert = false;
+
+  userSubscr: Subscription;
+  stationsSubscription: Subscription;
+  stationDetailSubscription: Subscription;
+
+  constructor(private authService: AuthenticationService,
+              private apiService: ExternalApiService) { }
 
   ngOnInit(): void {
     this.getUserData();
+    this.getStationData();
   }
 
   ngOnDestroy(): void {
@@ -32,6 +45,32 @@ export class AlertConfigurationComponent implements OnInit, OnDestroy {
           this.user = user;
         }
     });
+  }
+
+  getStationData(): void {
+    this.stationsSubscription = this.apiService.getAllStations()
+      .subscribe(stations => {
+        this.stations = stations;
+    });
+  }
+
+  getStationDetail(id: number): void {
+    this.stationDetailSubscription = this.apiService.getStationDetail(id)
+      .subscribe(detail => {
+        this.stationDetail = detail;
+    });
+  }
+
+  addStation(id: number): void {
+    console.log(id);
+    if (this.userStationList.length === 5) {
+      this.isListFullAlert = true;
+      return;
+    }
+    const selectedStation = this.stations.find(station => station.id === id);
+    if (selectedStation) {
+      this.userStationList.push(selectedStation);
+    }
   }
 
 }
--- src/app/components/station-map/station-map.component.ts
+++ src/app/components/station-map/station-map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExternalApiService } from '../../services/external-api.service';
 import { Station } from '../../model/Station';
 import { MeasurementPoint } from '../../model/MeasurementPoint';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-station-map',
@@ -13,19 +14,24 @@ export class StationMapComponent implements OnInit {
   stations: Array<Station> = [];
   stationDetail: Array<MeasurementPoint> = [];
 
+  stationsSubscription: Subscription;
+  stationDetailSubscription: Subscription;
+
   constructor(private apiService: ExternalApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getAllStations().subscribe(stations => {
-      this.stations = stations;
-    });
+    this.stationsSubscription = this.apiService.getAllStations()
+      .subscribe(stations => {
+        this.stations = stations;
+      });
   }
 
   getStationDetail(id: number): void {
-    this.apiService.getStationDetail(id).subscribe(detail => {
-      this.stationDetail = detail;
-      console.log(this.stationDetail);
-    });
+    this.stationDetailSubscription = this.apiService.getStationDetail(id)
+      .subscribe(detail => {
+        this.stationDetail = detail;
+        console.log(this.stationDetail);
+      });
   }
 
 }
